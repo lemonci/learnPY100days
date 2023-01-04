cabaa14f58bcce739e2189360a94451b1230709e
lemonci <hongyanchangfei@gmail.com>
2023-01-03 21:59:05 -0500

    Dynamic programming is left

---
+++ day16-20/simple_algorithms.py
@@ -0,0 +1,232 @@
+def select_sort(items, comp=lambda x, y: x < y):
+    """Simple sort"""
+    items = items[:]
+    for i in range(len(items)-1):
+        min_index = i
+        for j in range(i + 1, len(items)):
+            if comp(items[j], items[min_index]):
+                min_index = j
+        items[i], items[min_index] = items[min_index], items[i]
+    return items
+
+
+def bubble_sort(items, comp=lambda x, y: x > y):
+    """bubble sort"""
+    for i in range(len(items) - 1):
+        swapped = False
+        for j in range(len(items) - 1 - i):
+            if comp(items[j], items[j + 1]):
+                items[j], items[j + 1] = items[j + 1], items[j]
+                swapped = True
+            if not swapped:
+                break
+        return items
+
+
+def shake_sort(items, comp=lambda x, y: x > y):
+    """bidirectional bubble sort"""
+    items = items[:]
+    for i in range(len(items) - 1):
+        swapped = False
+        for j in range(len(items) - 1):
+            if comp(items[j], items[j + 1]):
+                items[j], items[j + 1] = items[j + 1], items[j]
+                swapped = True
+        if swapped:
+            swapped = False
+            for j in range(len(items) - 2 - i, i, -1):
+                if comp(items[j - 1], items[j]):
+                    items[j], items[j - 1] = items[j - 1], items[j]
+                    swapped = True
+        if not swapped:
+            break
+    return items
+
+
+def merge(items1, items2, comp=lambda x, y: x < y):
+    """Merger two sorted lists into one"""
+    items = []
+    index1, index2 = 0, 0
+    while index1 < len(items1) and index2 < len(items2):
+        if comp(items1[index1], items2[index2]):
+            items.append(items1[index1])
+            index1 += 1
+        else:
+            items.append((items2[index2]))
+            index2 += 1
+    items += items1[index1:]
+    items += items2[index2:]
+    return items
+
+
+def merge_sort(items, comp=lambda x, y: x < y):
+    return _merge_sort(list(items), comp)
+
+
+def _merge_sort(items, comp):
+    """Merge sort"""
+    if len(items) < 2:
+        return items
+    mid = len(items) // 2
+    left = _merge_sort(items[:mid], comp)
+    right = _merge_sort(items[mid:], comp)
+    return merge(left, right, comp)
+
+
+def seq_search(items, key):
+    """search according to original sequence"""
+    for index, item in enumerate(items):
+        if item == key:
+            return index
+    return -1
+
+
+def bin_search(items, key):
+    """Binary search"""
+    start, end = 0, len(items) - 1
+    while start <= end:
+        mid = (start + end) // 2
+        if key > items[mid]:
+            start = mid + 1
+        elif key < items[mid]:
+            end = mid - 1
+        else:
+            return mid
+    return -1
+
+
+# Brute Force
+# 1 $100 for 100 chicken
+for x in range(20):
+    for y in range(33):
+        z = 100 - x - y
+        if 5 * x + 3 * y + z // 3 == 100 and z % 3 == 0:
+            print(x, y, z)
+# 5 people divide fish
+fish = 6
+while True:
+    total = fish
+    enough = True
+    for _ in range(5):
+        if (total - 1) % 5 == 0:
+            total = (total - 1) // 5 * 4
+        else:
+            enough = False
+            break
+    if enough:
+        print(fish)
+        break
+    fish += 5
+
+
+# Knapsack problem
+class Thing(object):
+    """objects"""
+
+    def __init__(self, name, price, weight):
+        self.name = name
+        self.price = price
+        self.weight = weight
+
+    @property
+    def value(self):
+        """Price/Weight Ratio"""
+        return self.price / self.weight
+
+
+def input_thing():
+    """Input object information"""
+    name_str, price_str, weight_str = input().split()
+    return name_str, int(price_str), int(weight_str)
+
+
+def main():
+    """Main function"""
+    max_weight, num_of_things = map(int, input().split())
+    all_things = []
+    for _ in range(num_of_things):
+        all_things.append(Thing(*input_thing()))
+    all_things.sort(key=lambda x: x.value, reverse=True)
+    total_weight = 0
+    total_price = 0
+    for thing in all_things:
+        if total_weight + thing.weight <= max_weight:
+            print(f'{thing.name} is taken.')
+            total_weight += thing.weight
+            total_price += thing.price
+    print(f'Total value: $ {total_price}')
+
+
+if __name__ == '__main__':
+    main()
+
+
+"""Quick sort"""
+def quick_sort(items, comp=lambda x, y: x <= y):
+    items = list(items)[:]
+    _quick_sort(items, 0, len(items) - 1, comp)
+    return items
+
+
+def _quick_sort(items, start, end, comp):
+    if start < end:
+        pos = _partition(items, start, end, comp)
+        _quick_sort(items, start, pos - 1, comp)
+        _quick_sort(items, pos + 1, end, comp)
+
+
+def _partition(items, start, end, comp):
+    pivot = items[end]
+    i = start - 1
+    for j in range(start, end):
+        if comp(items[j], pivot):
+            i += 1
+            items[i], items[j] = items[j], items[i]
+    items[i + 1], items[end] = items[end], items[i + 1]
+    return i + 1
+
+
+"""
+Backtrack
+
+Knight's tour
+"""
+
+SIZE = 5
+total = 0
+
+
+def print_board(board):
+    for row in board:
+        for col in row:
+            print(str(col).center(4), end='')
+        print()
+
+
+def patrol(board, row, col, step=1):
+    if row >= 0 and row < SIZE and \
+            0 <= col < SIZE and \
+        board[row][col] == 0:
+        board[row][col] = step
+        if step == SIZE * SIZE:
+            global total
+            total += 1
+            print(f'Tour # {total}:')
+            print_board(board)
+        patrol(board, row - 2, col - 1, step + 1)
+        patrol(board, row - 1, col - 2, step + 1)
+        patrol(board, row + 1, col - 2, step + 1)
+        patrol(board, row + 2, col - 1, step + 1)
+        patrol(board, row + 2, col + 1, step + 1)
+        patrol(board, row + 1, col + 2, step + 1)
+        patrol(board, row - 1, col + 2, step + 1)
+        patrol(board, row - 2, col + 1, step + 1)
+        board[row][col] = 0
+
+
+def main():
+    board = [[0] * SIZE for _ in range(SIZE)]
+    patrol(board, SIZE - 1, SIZE - 1)
+
+if __name__ == '__main__':
+    main()
